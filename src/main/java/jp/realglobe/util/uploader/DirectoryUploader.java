335b1bd87bc0c0b3e449055322d77d58f83c34aa
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2016-11-02 14:32:17 +0900

    ID とトークンを指定できるようにした

--- src/main/java/jp/realglobe/util/uploader/DirectoryUploader.java
+++ src/main/java/jp/realglobe/util/uploader/DirectoryUploader.java
@@ -57,6 +57,43 @@ public class DirectoryUploader implements Runnable {
     private final URI uploadUrl;
     // 運用データの保管庫
     private final Store store;
+    // 認証トークン
+    private final String token;
+
+    /**
+     * 作成する
+     * @param watchDirectoryPath 監視するディレクトリのパス
+     * @param delay ファイルの変更からアップロードまでの猶予期間（ミリ秒）。
+     *            細かい更新に対して毎回アップロードを行わないため
+     * @param latestOnly 処理落ち中に検知したファイルをスキップするか
+     * @param targetExtensions アップロード対象の拡張子
+     * @param minSize アップロード対象の最小ファイルサイズ（バイト）
+     * @param maxSize アップロード対象の最大ファイルサイズ（バイト）
+     * @param urlBase アップロード先サーバーの URL
+     * @param userId 紐付くユーザーの ID
+     * @param name 表示名
+     * @param id 自身の ID
+     * @param token 認証トークン
+     * @throws Exception データ読み書きエラー
+     */
+    public DirectoryUploader(final Path watchDirectoryPath, final long delay, final boolean latestOnly, final Collection<String> targetExtensions, final long minSize, final long maxSize,
+            final String urlBase, final String userId, final String name, final String id, final String token) throws Exception {
+        this.watchDirectoryPath = watchDirectoryPath;
+        this.delay = delay;
+        this.latestOnly = latestOnly;
+        this.targetExtensions = (targetExtensions == null ? Collections.emptySet() : new HashSet<>(targetExtensions));
+        this.minSize = minSize;
+        this.maxSize = maxSize;
+        this.userId = userId;
+        this.id = id;
+        this.name = name;
+        this.tokenUrl = null;
+        this.uploadUrl = (new URL(urlBase + Constants.URL_PATH_UPLOAD_PREFIX + "/" + this.id + Constants.URL_PATH_UPLOAD_SUFFIX)).toURI();
+        this.store = null;
+        this.token = token;
+
+        LOG.info("ID is " + this.id);
+    }
 
     /**
      * 作成する
@@ -87,6 +124,7 @@ public class DirectoryUploader implements Runnable {
         this.tokenUrl = (new URL(urlBase + Constants.URL_PATH_TOKEN)).toURI();
         this.uploadUrl = (new URL(urlBase + Constants.URL_PATH_UPLOAD_PREFIX + "/" + this.id + Constants.URL_PATH_UPLOAD_SUFFIX)).toURI();
         this.store = store;
+        this.token = null;
 
         LOG.info("ID is " + this.id);
     }
@@ -113,17 +151,21 @@ public class DirectoryUploader implements Runnable {
 
     @Override
     public void run() {
-        final String token;
-        try {
-            token = getToken();
-        } catch (final InterruptedException e) {
-            // 終了
-            return;
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
+        final String token1;
+        if (this.token != null) {
+            token1 = this.token;
+        } else {
+            try {
+                token1 = getToken();
+            } catch (final InterruptedException e) {
+                // 終了
+                return;
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
         }
 
-        LOG.info("Use token " + token);
+        LOG.info("Use token " + token1);
 
         (new DelayedWatcher(this.watchDirectoryPath, this.delay, this.latestOnly, path -> {
             if (!Files.isReadable(path)) {
@@ -141,7 +183,7 @@ public class DirectoryUploader implements Runnable {
                 LOG.info("Skip too large file " + path);
                 return;
             }
-            upload(token, path);
+            upload(token1, path);
         })).run();
     }
 
@@ -159,12 +201,12 @@ public class DirectoryUploader implements Runnable {
             }
             this.store.clear(Constants.STORE_KEY_TOKEN);
         }
-        final String token = getRemoteToken();
-        if (token == null) {
+        final String token1 = getRemoteToken();
+        if (token1 == null) {
             throw new RuntimeException("Cannot get token");
         }
-        this.store.store(Constants.STORE_KEY_TOKEN, token);
-        return token;
+        this.store.store(Constants.STORE_KEY_TOKEN, token1);
+        return token1;
     }
 
     /**
@@ -172,7 +214,7 @@ public class DirectoryUploader implements Runnable {
      * @param token 調べる認証トークン
      * @return 有効なら true
      */
-    private boolean isValidToken(final String token) {
+    private boolean isValidToken(final String token1) {
         // TODO 実際にサーバーにつなげて検査
         LOG.info("Token check is not implemented");
         return true;
@@ -203,18 +245,18 @@ public class DirectoryUploader implements Runnable {
 
     /**
      * ファイルをアップロードする
-     * @param token 認証トークン
+     * @param token1 認証トークン
      * @param path アップロードするファイルのパス
      * @throws IOException ファイルが無かったり、通信異常だったり
      * @throws HttpException HTTP エラー
      */
-    private void upload(final String token, final Path path) throws HttpException, IOException {
+    private void upload(final String token1, final Path path) throws HttpException, IOException {
         try (CloseableHttpClient client = HttpClients.createDefault()) {
 
             final HttpPost post = new HttpPost(this.uploadUrl);
             post.setEntity(MultipartEntityBuilder.create()
                     .addTextBody("info", "{}")
-                    .addTextBody(Constants.UPLOAD_REQUEST_PART_TOKEN, token)
+                    .addTextBody(Constants.UPLOAD_REQUEST_PART_TOKEN, token1)
                     .addBinaryBody(Constants.UPLOAD_REQUEST_PART_DATA, path.toFile())
                     .build());
 
